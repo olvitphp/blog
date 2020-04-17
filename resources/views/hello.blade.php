04553264ce7a5cdd2f06bbb7179d73a0c5fd8967
olvit <olvit2008@ukr.net>
2020-04-17 21:53:33 +0300

    Vidio_39

--- resources/views/hello.blade.php
+++ resources/views/hello.blade.php
@@ -4,18 +4,15 @@
 @section('content')
 
 
-<hr>
-<button type="button" class="btn btn-primary"><a href="http://127.0.0.1:8000">Старт</a></button>
-<button type="button" class="btn btn-secondary"><a href="http://127.0.0.1:8000/blog/posts">Посты</a></button>
-<button type="button" class="btn btn-success">Success</button>
-<button type="button" class="btn btn-danger">Danger</button>
-<button type="button" class="btn btn-warning">Warning</button>
-<button type="button" class="btn btn-info">Info</button>
-<button type="button" class="btn btn-light">Light</button>
-<button type="button" class="btn btn-dark">Dark</button>
-
-<button type="button" class="btn btn-link">Link</button>
-
+    <div class="card" style="width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="card-link">Card link</a>
+            <a href="#" class="card-link">Another link</a>
+        </div>
+    </div>
 
 
 @endsection
