8b30608e076924ec155187f1d6188e96077c0ebb
olvit <olvit2008@ukr.net>
2020-04-18 21:30:25 +0300

    Vidio_40

--- app/Models/BlogPost.php
+++ app/Models/BlogPost.php
@@ -2,28 +2,78 @@
 
 namespace App\Models;
 
+
+
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
+
+/**
+ * Class BlogPost
+ * @package App\Models
+ *
+ * @property BlogCategory $category
+ * @property User                      $user
+ * @property string                    $title
+ * @property string                    $slug
+ * @property string                    $content_html
+ * @property string                    $content_raw
+ * @property string                    $excerpt
+ * @property string                    $published_it
+ * @property string                    $is_published
+ *
+ */
 
 class BlogPost extends Model
 {
     use SoftDeletes;
 
-    /**
-     * @return BelongsTo
-     */
+    const UNKNOWN_USER =1;
 
+    protected $fillable
+        = [
+            'title',
+            'slug',
+            'category_id',
+            'excerpt',
+            'content_raw',
+            'is_published',
+            'published_at',
+
+        ];
+
+    /**
+     * Кктегория статьи.
+     *
+     * @return BelongsTo
+     *
+     */
     public function category()
     {
+        // СТАТЬИ ПРИНАДЛЕЖАТ КАТЕГОРИИ
         return $this->belongsTo(BlogCategory::class);
     }
 
     /**
+     *  // Автор статьи.
+     *
      * @return BelongsTo
      */
+
     public function user()
     {
+        // Статья принадлежит пользователю
         return $this->belongsTo(User::class);
     }
+
+    /**
+     * @inheritDoc
+     */
+    public function resolveChildRouteBinding($childType, $value, $field)
+    {
+        // TODO: Implement resolveChildRouteBinding() method.
+    }
+
+
+
 }
